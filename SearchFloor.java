abd1448414cba42b403cab7c56e24fbad9138adf
Oj2AS7 <ojas.271001@gmail.com>
2025-11-23 23:22:28 +0530

    All the question I have solved regarding the binary search

--- SearchFloor.java
+++ SearchFloor.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ~SearchFloor {
+public class SearchFloor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
